385775b19867c80ad0f35522536cf6ef15d283e7
acadavido <andraeacadavidosorio@gmail.com>
2022-03-23 20:50:27 -0500

    Video 127

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 function App() {
   const [gastos, setGastos]  = useState([])
 
-  const [presupuesto, setPresupuesto] = useState(0)
+  const [presupuesto, setPresupuesto] = useState(
+    localStorage.getItem('presupuesto') ?? 0
+  )
   const [isValidPresupuesto, setIsValidPresupuesto]=useState(false)
   
   const [modal, setModal] = useState(false)
@@ -26,6 +28,17 @@ function App() {
     }
   }, [gastoEditar]) 
 
+  useEffect(() => {
+      Number(localStorage.setItem('presupuesto', presupuesto ?? 0))
+  }, [presupuesto])
+
+  useEffect( () =>{
+    const presupuestoLS=Number(localStorage.getItem('presupuesto')) ?? 0;
+ 
+    if(presupuestoLS > 0) { 
+      setIsValidPresupuesto(true)
+    }
+  }, [])
 
   const handleNuevoGasto = () => {
     setModal(true)
--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react"
-import {CircularProgressbar} from 'react-circular-progressbar' 
+import {CircularProgressbar, buildStyles} from 'react-circular-progressbar' 
 import 'react-circular-progressbar/dist/styles.css'
 const ControlPresupuesto = ({presupuesto, gastos}) => {
 
+  const [porcentaje, setPorcentaje] = useState(0)
   const [disponible, setDisponible]= useState(0)
   const [gastado, setGastado] = useState(0)
 
@@ -10,8 +11,15 @@ const ControlPresupuesto = ({presupuesto, gastos}) => {
     const totalGastado = gastos.reduce( (total, gasto) => gasto.cantidad + total, 0)
     const totalDisponible = presupuesto - totalGastado
    
+    // Calcular el porcentaje gastado
+    const nuevoPorcentaje = (( (presupuesto - totalDisponible) / presupuesto )*100).toFixed(2)
+    
+
     setDisponible(totalDisponible)
     setGastado(totalGastado)
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje)
+    },1500);
 
   }, [gastos])
 
@@ -28,7 +36,13 @@ const ControlPresupuesto = ({presupuesto, gastos}) => {
     <div className="contenedor-presupuesto contenedor sombra dos-columnas"> 
       <div>
           <CircularProgressbar
-          value={50}
+          styles={buildStyles({
+              pathColor: '#3B82F6',
+              trailColor: '#F5F5F5',
+              textColor: '#3B82F6',
+          })}
+          value={porcentaje}
+          text={`${porcentaje}% Gastado`}
           />
       </div>
 
